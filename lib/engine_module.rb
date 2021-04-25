e76dc89ce94f0c3854b69b9b1e41f5954290a9d9
Lawrence Whalen <ldwhalen@gmail.com>
2021-04-25 17:43:36 -0600

    Green gererate offset and Red character_shuffle

---
+++ lib/engine_module.rb
@@ -0,0 +1,10 @@
+module EngineModule
+  def generate_offset(key, offset)
+    squared_offset = (offset.to_i ** 2).to_s
+    justified_key = key.rjust(5, "0")
+    {0 => ((justified_key[0] + justified_key[1]).to_i + offset[-4].to_i) % 28,
+     1 => ((justified_key[1] + justified_key[2]).to_i + offset[-3].to_i) % 28,
+     2 => ((justified_key[2] + justified_key[3]).to_i + offset[-2].to_i) % 28,
+     3 => ((justified_key[3] + justified_key[4]).to_i + offset[-1].to_i) % 28}
+  end
+end
--- spec/engine_module_spec.rb
+++ spec/engine_module_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe 'EngineModule' do
       expect(mock_class.generate_offset('02715', '040895')).to eq(expected)
     end
   end
+  describe '#character_shuffle' do
+    let(:mock_class) { Class.new { extend EngineModule } }
+
+    it 'takes a hash and encrypts a sting if passed nil' do
+      actual_encryption = mock_class.character_shuffle(message: 'hello world',
+                                                       offset: {0 => 2, 1 => 7, 2 => 24, 3 => 20},
+                                                       crypt: :+)
+
+      expect(actual_encryption.class).to eq(String)
+      expect(actual_encryption).to eq('jlieqgthtsa')
+    end
+  end
 end
