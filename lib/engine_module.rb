8955f48f1e73e1f94d52b36355a29e5272e55745
Lawrence Whalen <ldwhalen@gmail.com>
2021-04-26 12:02:43 -0600

    Refactor generate offset to produce an array for clarity

--- lib/engine_module.rb
+++ lib/engine_module.rb
@@ -2,18 +2,17 @@ module EngineModule
   def generate_offset(key, offset)
     squared_offset = (offset.to_i ** 2).to_s
     justified_key = key.rjust(5, "0")
-    #likely be an array
-    {0 => ((justified_key[0] + justified_key[1]).to_i + offset[-4].to_i) % 28,
-     1 => ((justified_key[1] + justified_key[2]).to_i + offset[-3].to_i) % 28,
-     2 => ((justified_key[2] + justified_key[3]).to_i + offset[-2].to_i) % 28,
-     3 => ((justified_key[3] + justified_key[4]).to_i + offset[-1].to_i) % 28}
+    [((justified_key[0] + justified_key[1]).to_i + offset[-4].to_i) % 28,
+     ((justified_key[1] + justified_key[2]).to_i + offset[-3].to_i) % 28,
+     ((justified_key[2] + justified_key[3]).to_i + offset[-2].to_i) % 28,
+     ((justified_key[3] + justified_key[4]).to_i + offset[-1].to_i) % 28]
   end
   def character_shuffle(cryption_hash)
-    offset_hash = cryption_hash[:offset]
+    offset_array = cryption_hash[:offset]
     cryption_hash[:message].split('').map.with_index do |character, index|
       if @alphabet.include?(character)
         character_location = @alphabet.find_index(character)
-        offset_index = cryption_hash[:crypt] * (offset_hash[index % 4])
+        offset_index = cryption_hash[:crypt] * (offset_array[index % 4])
         @alphabet.rotate(character_location)[offset_index]
       else
         character
