8243c3a374240a7dd43dc197024d58f75190278d
Lawrence Whalen <ldwhalen@gmail.com>
2021-04-25 22:13:36 -0600

    Green message check and Red offset check

--- lib/security.rb
+++ lib/security.rb
@@ -1,7 +1,14 @@
 class Security
   def self.check_input(security_hash)
-    message_check(security_hash[:message])
-    offset_check(security_hash[:offset])
-    date_check(security_hash[:date])
+    Security.message_check(security_hash[:message])
+    # Security.offset_check(security_hash[:offset])
+    # Security.date_check(security_hash[:date])
+  end
+  def self.message_check(message)
+    if message.class != String
+      puts 'Your message cannot be translated!'
+      exit
+    end
+    true
   end
 end
--- spec/security_spec.rb
+++ spec/security_spec.rb
@@ -10,20 +10,30 @@ RSpec.describe 'Security' do
   end
   describe '#check_input' do
     it 'calls on the three methods to check the input' do
-      allow(Security).to receive(:message_check).and_return('Tim')
-      allow(Security).to receive(:offset_check).and_return('90341')
-      allow(Security).to receive(:messsage_check).and_return('240185')
+      expect(Security).to receive(:message_check).with('Tim')
+      # expect(Security).to receive(:key_check).with('90341')
+      # expect(Security).to receive(:date_check).with('240185')
 
-      actual = Security.check_input(message: 'Tim',offset: '90341',date: '240185')
-
-      expect(actual).to eq(message: 'Tim', key: '90341', date: '240185')
+      Security.check_input(message: 'Tim',offset: '90341',date: '240185')
     end
   end
   describe '#message_check' do
     it 'ensures that the passed message is a string' do
       actual = Security.message_check('Time Seen')
 
-      expect(actual).to eq('Time Seen')
+      expect(actual).to eq(true)
+    end
+    it 'exits if the passed parameter is not a string' do
+
+      expect{Security.message_check(90909)}.to raise_error(SystemExit)
+
+    end
+  end
+  describe '#offset_check' do
+    it 'ensures that the offset passed falls within basic parameters' do
+      actual = Security.offset_check('09090')
+
+      expect(actual).to eq(true)
     end
   end
 end
