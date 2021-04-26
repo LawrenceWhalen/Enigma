5246efc63714bb365c8cfdd495d69fdd5805c693
Lawrence Whalen <ldwhalen@gmail.com>
2021-04-26 00:16:36 -0600

    Fix the SystemExit tests

--- lib/security.rb
+++ lib/security.rb
@@ -1,12 +1,25 @@
 class Security
   def self.check_input(security_hash)
     Security.message_check(security_hash[:message])
-    # Security.offset_check(security_hash[:offset])
+    Security.key_check(security_hash[:key])
     # Security.date_check(security_hash[:date])
   end
   def self.message_check(message)
     if message.class != String
-      puts 'Your message cannot be translated!'
+      print 'Your message cannot be translated!'
+      exit
+    end
+    true
+  end
+  def self.key_check(key)
+    if key.class != String
+      print 'The key is passed incorrectly!'
+      exit
+    elsif (key.to_i.to_s).rjust(5, '0') != key
+      print 'The key is not a number!'
+      exit
+    elsif key.length > 5
+      print 'The key is too long!'
       exit
     end
     true
--- spec/security_spec.rb
+++ spec/security_spec.rb
@@ -11,10 +11,10 @@ RSpec.describe 'Security' do
   describe '#check_input' do
     it 'calls on the three methods to check the input' do
       expect(Security).to receive(:message_check).with('Tim')
-      # expect(Security).to receive(:key_check).with('90341')
+      expect(Security).to receive(:key_check).with('90341')
       # expect(Security).to receive(:date_check).with('240185')
 
-      Security.check_input(message: 'Tim',offset: '90341',date: '240185')
+      Security.check_input(message: 'Tim',key: '90341',date: '240185')
     end
   end
   describe '#message_check' do
@@ -24,16 +24,71 @@ RSpec.describe 'Security' do
       expect(actual).to eq(true)
     end
     it 'exits if the passed parameter is not a string' do
-
-      expect{Security.message_check(90909)}.to raise_error(SystemExit)
-
+      expect {
+        begin Security.message_check(90909)
+        rescue SystemExit
+        end
+      }.to output('Your message cannot be translated!').to_stdout
     end
   end
-  describe '#offset_check' do
-    it 'ensures that the offset passed falls within basic parameters' do
-      actual = Security.offset_check('09090')
+  describe '#key_check' do
+    it 'ensures that the key passed falls within basic parameters' do
+      actual = Security.key_check('01010')
 
       expect(actual).to eq(true)
     end
+    it 'exits if the passed key is not a string' do
+      expect {
+        begin Security.key_check(90909)
+        rescue SystemExit
+        end
+      }.to output('The key is passed incorrectly!').to_stdout
+    end
+    it 'exits if the passed key is not a set of numbers' do
+      expect {
+        begin Security.key_check('P0909')
+        rescue SystemExit
+        end
+      }.to output('The key is not a number!').to_stdout
+    end
+    it 'exits if the passed key is too long' do
+      expect {
+        begin Security.key_check('909099')
+        rescue SystemExit
+        end
+      }.to output('The key is too long!').to_stdout
+    end
   end
+  # describe '#date_check' do
+  #   it 'ensures the date is correctly formatted' do
+  #     actual = Security.date_check('010121')
+  #
+  #     expect(actual).to eq(true)
+  #   end
+  #   it 'exits if the passed date is not a string' do
+  #
+  #     expect{Security.date_check(010121)}.to raise_error(SystemExit)
+  #
+  #   end
+  #   it 'exits if the passed date is not a number' do
+  #
+  #     expect{Security.date_check('P10121')}.to raise_error(SystemExit)
+  #
+  #   end
+  #   it 'exits if the passed date is too long' do
+  #
+  #     expect{Security.date_check('0101211')}.to raise_error(SystemExit)
+  #
+  #   end
+  #   it 'exits if the passed month is incorrectly formatted' do
+  #
+  #     expect{Security.date_check('011321')}.to raise_error(SystemExit)
+  #
+  #   end
+  #   it 'exits if the passed day is impossible for the month' do
+  #
+  #     expect{Security.date_check('300221')}.to raise_error(SystemExit)
+  #
+  #   end
+  # end
 end
